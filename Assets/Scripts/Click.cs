66a7d039df629b2301f61a855d733a40ba6d9c68
Biel <vgabriel@id.uff.br>
2020-05-06 17:58:48 -0300

    Adicionado TextMeshPro e uma botão de click

---
+++ Assets/Scripts/Click.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Click : MonoBehaviour
+{
+    private int num = 0;
+
+    private static float count = 0.8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Clicked(TextMeshProUGUI pontos)
+    {
+        num++;
+        pontos.fontSize = 45;
+        pontos.text = num.ToString();
+        count-= Time.deltaTime;
+        if (count <= 0)
+        {
+            pontos.fontSize = 36;
+            count = 0.8f;
+        }
+    }
+}
