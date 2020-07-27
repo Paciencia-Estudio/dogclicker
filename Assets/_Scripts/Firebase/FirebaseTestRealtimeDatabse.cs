24a53982626923c72c957f9ebfacd453eca817bc
Biel <vgabriel@id.uff.br>
2020-07-27 13:15:58 -0300

    Primeiras tentativas do RTD;

---
+++ Assets/_Scripts/Firebase/FirebaseTestRealtimeDatabse.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+
+public class FirebaseTestRealtimeDatabse : MonoBehaviour
+{
+    DatabaseReference reference;
+    void Start()
+    {
+        // Set this before calling into the realtime database.
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://dogvillage-66857.firebaseio.com/");
+
+        // Get the root reference location of the database.
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void EnviarParaRTD(int pontos)
+    {
+        //reference.SetValueAsync(pontos)
+    }
+}
