f07ed575c8d95574ca86441832cd362060e88d32
Biel <olarbiel@gmail.com>
2025-06-27 18:06:44 -0300

    Corrigidas apis do Firebase que estavam com pacotes ultrapassados e também escrita não atual;

--- Assets/_Scripts/_CloudSaveManager.cs
+++ Assets/_Scripts/_CloudSaveManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Firebase;
 using Firebase.Database;
-using Firebase.Unity.Editor;
+using Firebase.Unity;
 using System;
 using Firebase.Analytics;
 
